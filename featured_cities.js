f8994426337fd601097e0d01eec9e893ed58758d
Charles Broskoski <broskoski@gmail.com>
2014-12-08 16:16:31 -0500

    update featured cities to return an array of ordered cities

--- featured_cities.js
+++ featured_cities.js
@@ -1,20 +1,17 @@
 var _ = require('underscore');
 var Cities = require('./cities.js');
 
-var selected = [
-  'new-york',
-  'london',
-  'los-angeles',
-  'paris',
-  'berlin',
-  'miami',
-  'san-francisco',
-  'hong-kong',
-  'milan',
-  'tokyo'
+module.exports = [
+  _.findWhere(Cities, {slug:'new-york'}),
+  _.findWhere(Cities, {slug:'london'}),
+  _.findWhere(Cities, {slug:'los-angeles'}),
+  _.findWhere(Cities, {slug:'paris'}),
+  _.findWhere(Cities, {slug:'berlin'}),
+  _.findWhere(Cities, {slug:'miami'}),
+  _.findWhere(Cities, {slug:'san-francisco'}),
+  _.findWhere(Cities, {slug:'hong-kong'}),
+  _.findWhere(Cities, {slug:'milano'}),
+  _.findWhere(Cities, {slug:'sao-paolo'}),
+  _.findWhere(Cities, {slug:'tokyo'})
 ];
 
-module.exports =
-  _.filter(Cities, function( city ){
-    return _.contains(selected, city.slug);
-  });
